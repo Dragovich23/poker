24fc8de02022f0acf3c671dbe0a7a9d0be938c23
Dragovich23 <69821313+Dragovich23@users.noreply.github.com>
2023-03-26 17:39:27 +0200

    Add files via upload

--- 14.03.23/scripts/main.js
+++ 14.03.23/scripts/main.js
@@ -5,8 +5,9 @@ $("div.lista").mouseover(function() {
 $("div.lista").mouseleave(function() {
     $("ul").hide(300)
 });
-$('[href="#"]:eq(4)').attr('href', './contact.html'); 
-$('[href="#"]:eq(3)').attr('href', './footer.html'); 
+$('[href="#"]:eq(5)').attr('href', './contact.html'); 
+$('[href="#"]:eq(4)').attr('href', './footer.html'); 
+$('[href="#"]:eq(3)').attr('href', '#');
 $('[href="#"]:eq(2)').attr('href', '#');
 $('[href="#"]:eq(1)').attr('href', './uklady.html');
 $('[href="#"]:eq(0)').attr('href', './index.html');
