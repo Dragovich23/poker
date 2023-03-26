abb2f768e31e1b5a12e5b8aca2f5ad0b43cffd49
Dragovich23 <69821313+Dragovich23@users.noreply.github.com>
2023-03-26 17:38:51 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+
+$("div.lista").mouseover(function() {
+    $("ul").show(300)
+});
+$("div.lista").mouseleave(function() {
+    $("ul").hide(300)
+});
+$('[href="#"]:eq(5)').attr('href', './contact.html'); 
+$('[href="#"]:eq(4)').attr('href', './footer.html'); 
+$('[href="#"]:eq(3)').attr('href', '#');
+$('[href="#"]:eq(2)').attr('href', '#');
+$('[href="#"]:eq(1)').attr('href', './uklady.html');
+$('[href="#"]:eq(0)').attr('href', './index.html');
+
+
+
